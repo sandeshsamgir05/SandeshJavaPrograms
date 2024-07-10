a01d8bef45a63e2f6d31899d761ef3455a56598b
sandeshsamgir05 <153384366+sandeshsamgir05@users.noreply.github.com>
2024-07-10 23:43:42 +0530

    1st commit

---
+++ src/Day13/StaticDemo.java
@@ -0,0 +1,29 @@
+package Day13;
+
+public class StaticDemo {
+	
+	
+	static int a =10;
+	int b =20;
+	
+	static void m1()
+	{
+		System.out.println("this is m1 static method...");
+		
+	}
+	
+	void m2()
+	
+	{
+		System.out.println("thi");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	
+		
+
+	}
+
+}
