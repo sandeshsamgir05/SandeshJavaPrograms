f53c27ff5578e4471f4a552d0a0bd99796c46742
sandeshsamgir05 <153384366+sandeshsamgir05@users.noreply.github.com>
2024-07-11 21:24:56 +0530

    test

--- src/Day13/StaticDemo.java
+++ src/Day13/StaticDemo.java
@@ -15,12 +15,19 @@ public class StaticDemo {
 	void m2()
 	
 	{
-		System.out.println("thi");
+		System.out.println("this is m2 non-static method");
 	}
-
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+	System.out.println(a);
+	m1();
+	//System.out.println(b); We can not access bcz b is no static
+	//m2(); We can not access bcz m2 is no static method
+	
+	StaticDemo sd = new StaticDemo();
+	System.out.println(sd.b);
+	sd.m2();
 	
 		
 
